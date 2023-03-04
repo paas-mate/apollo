071e0c5b502c3754c52b546bcf4e770698a458f8
goflutterjava <goflutterjava@gmail.com>
2023-03-04 17:12:43 +0800

    fix shell sleep

--- docker-build/scripts/start.sh
+++ docker-build/scripts/start.sh
@@ -5,9 +5,9 @@ DIR="$( cd "$( dirname "$0"  )" && pwd  )"
 mkdir -p $APOLLO_HOME/logs
 
 bash -x $DIR/start-config.sh
-sleep(10_000)
+sleep 10
 bash -x $DIR/start-admin.sh
-sleep(10_000)
+sleep 10
 bash -x $DIR/start-portal.sh
 
 tail -f /dev/null
