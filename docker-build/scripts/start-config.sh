4cd43e02a35418e03a5715a67f9fd8949ed77f35
lovehzj <1922919664@qq.com>
2023-03-05 17:16:59 +0800

    optimize code

--- docker-build/scripts/start-config.sh
+++ docker-build/scripts/start-config.sh
@@ -4,10 +4,9 @@ LOG_FILE="$APOLLO_LOG_DIR/config.out"
 if ! [ -z ${RDS_TYPE} ]; then
     echo "spring.profiles.group.github = $RDS_TYPE" >${CONFIG_SERVICE_DIR}/application.properties    
 fi
-nohup java  \
-    -Dapollo_profile=${APOLLO_PROFILE} \
-    -Dspring.datasource.url=${DATASOURCE_URL} \
-    -Dspring.datasource.username=${DATASOURCE_USERNAME} \
-    -Dspring.datasource.password=${DATASOURCE_PASSWORD} \
-    -Dspring.jpa.database-platform=${DATASOURCE_PLATEFORM} \
-    -jar ${CONFIG_SERVICE_DIR}/apollo-configservice.jar 2>&1 >$LOG_FILE &
+OPTS="$OPTS -Dapollo_profile=${APOLLO_PROFILE}"
+OPTS="$OPTS -Dspring.datasource.url=${DATASOURCE_URL}"
+OPTS="$OPTS -Dspring.datasource.username=${DATASOURCE_USERNAME}"
+OPTS="$OPTS -Dspring.datasource.password=${DATASOURCE_PASSWORD}"
+OPTS="$OPTS -Dspring.jpa.database-platform=${DATASOURCE_PLATEFORM}"
+nohup java -jar ${CONFIG_SERVICE_DIR}/apollo-configservice.jar 2>&1 >$LOG_FILE &
