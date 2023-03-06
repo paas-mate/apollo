59d14c2061ee8e880ed08c797d331c5e511a89a3
Lico-Tom <583566138@qq.com>
2023-03-06 11:05:39 +0800

    add portal datasource.

--- docker-build/scripts/start-portal.sh
+++ docker-build/scripts/start-portal.sh
@@ -4,10 +4,19 @@ LOG_FILE="$APOLLO_LOG_DIR/portal.out"
 if ! [ -z ${RDS_TYPE} ]; then
     OPTS="$OPTS -Dspring.profiles.group.github=${RDS_TYPE}"
 fi
-OPTS="$OPTS -Ddev_meta=http://localhost:8080/"
-OPTS="$OPTS -Dapollo_profile=${APOLLO_PROFILE}"
-OPTS="$OPTS -Dspring.datasource.url=${DATASOURCE_URL}"
-OPTS="$OPTS -Dspring.datasource.username=${DATASOURCE_USERNAME}"
-OPTS="$OPTS -Dspring.datasource.password=${DATASOURCE_PASSWORD}"
-OPTS="$OPTS -Dspring.jpa.database-platform=${DATASOURCE_PLATEFORM}"
+if [ -z ${PORTAL_DATASOURCE_URL} ] && [ -z ${PORTAL_DATASOURCE_USERNAME} ] ; then
+  OPTS="$OPTS -Ddev_meta=http://localhost:8080/"
+  OPTS="$OPTS -Dapollo_profile=${APOLLO_PROFILE}"
+  OPTS="$OPTS -Dspring.datasource.url=${DATASOURCE_URL}"
+  OPTS="$OPTS -Dspring.datasource.username=${DATASOURCE_USERNAME}"
+  OPTS="$OPTS -Dspring.datasource.password=${DATASOURCE_PASSWORD}"
+  OPTS="$OPTS -Dspring.jpa.database-platform=${DATASOURCE_PLATEFORM}"
+else
+  OPTS="$OPTS -Ddev_meta=http://localhost:8080/"
+  OPTS="$OPTS -Dapollo_profile=${APOLLO_PROFILE}"
+  OPTS="$OPTS -Dspring.datasource.url=${PORTAL_DATASOURCE_URL}"
+  OPTS="$OPTS -Dspring.datasource.username=${PORTAL_DATASOURCE_USERNAME}"
+  OPTS="$OPTS -Dspring.datasource.password=${PORTAL_DATASOURCE_PASSWORD}"
+  OPTS="$OPTS -Dspring.jpa.database-platform=${PORTAL_DATASOURCE_PLATEFORM}"
+fi
 nohup java $OPTS -jar ${PORTAL_SERVICE_DIR}/apollo-portal.jar 2>&1 >$LOG_FILE &
