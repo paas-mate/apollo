818a48ea502a58eb92a83e67dd2aaf0ca09a28a5
goflutterjava <goflutterjava@gmail.com>
2023-03-07 08:14:39 +0800

    add judge datasource platform is empty

--- docker-build/scripts/start-admin.sh
+++ docker-build/scripts/start-admin.sh
@@ -8,5 +8,7 @@ OPTS="$OPTS -Dapollo_profile=${APOLLO_PROFILE}"
 OPTS="$OPTS -Dspring.datasource.url=${DATASOURCE_URL}"
 OPTS="$OPTS -Dspring.datasource.username=${DATASOURCE_USERNAME}"
 OPTS="$OPTS -Dspring.datasource.password=${DATASOURCE_PASSWORD}"
-OPTS="$OPTS -Dspring.jpa.database-platform=${DATASOURCE_PLATEFORM}"
+if ! [ -z ${DATASOURCE_PLATFORM} ]; then
+    OPTS="$OPTS -Dspring.jpa.database-platform=${DATASOURCE_PLATFORM}"
+fi
 nohup java $OPTS -jar ${ADMIN_SERVICE_DIR}/apollo-adminservice.jar 2>&1 >$LOG_FILE &
--- docker-build/scripts/start-portal.sh
+++ docker-build/scripts/start-portal.sh
@@ -9,13 +9,14 @@ if [ -z ${PORTAL_DATASOURCE_URL} ] && [ -z ${PORTAL_DATASOURCE_USERNAME} ] ; the
   OPTS="$OPTS -Dspring.datasource.url=${DATASOURCE_URL}"
   OPTS="$OPTS -Dspring.datasource.username=${DATASOURCE_USERNAME}"
   OPTS="$OPTS -Dspring.datasource.password=${DATASOURCE_PASSWORD}"
-  OPTS="$OPTS -Dspring.jpa.database-platform=${DATASOURCE_PLATEFORM}"
 else
   OPTS="$OPTS -Dapollo_profile=${APOLLO_PROFILE}"
   OPTS="$OPTS -Dspring.datasource.url=${PORTAL_DATASOURCE_URL}"
   OPTS="$OPTS -Dspring.datasource.username=${PORTAL_DATASOURCE_USERNAME}"
   OPTS="$OPTS -Dspring.datasource.password=${PORTAL_DATASOURCE_PASSWORD}"
-  OPTS="$OPTS -Dspring.jpa.database-platform=${PORTAL_DATASOURCE_PLATEFORM}"
+fi
+if ! [ -z ${DATASOURCE_PLATFORM} ]; then
+    OPTS="$OPTS -Dspring.jpa.database-platform=${DATASOURCE_PLATFORM}"
 fi
 OPTS="$OPTS -Ddev_meta=http://localhost:8080/"
 nohup java $OPTS -jar ${PORTAL_SERVICE_DIR}/apollo-portal.jar 2>&1 >$LOG_FILE &
